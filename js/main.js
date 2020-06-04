abec0d140fe8e487de714361d05a9d8530eea5f8
iricardo97 <ricardodomingos97@gmail.com>
2020-06-04 09:26:22 -0300

    aula 1 introdução ao javascript codigos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 var nome = "ricardo domingos"
 var idade = 38
+var idade2 = 10
+var n1 = 12
+var n2 = 20
+var frase = "Japao é o melhor time do mundo"
 //alert(nome + " tem " + idade + " anos ")
+console.log(nome)
+console.log(idade)
+console.log(frase)
+//console.log(frase.replace("Japao", "Brasil"))
+//alert(frase.replace("Japao", "Brasil"))
+//console.log(frase.toUpperCase())
+console.log(frase.toLowerCase())
+//console.log(idade + idade2)
+console.log(n1 + n2)
