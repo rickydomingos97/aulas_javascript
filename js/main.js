0b8bd636d0eef0ff82b0defeb0acbd0ad5845b91
iricardo97 <ricardodomingos97@gmail.com>
2020-06-04 09:52:36 -0300

    codigos aula 2 add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* CODIGOS AULA 1
 var nome = "ricardo domingos"
 var idade = 38
 var idade2 = 10
@@ -13,4 +14,36 @@ console.log(frase)
 //console.log(frase.toUpperCase())
 console.log(frase.toLowerCase())
 //console.log(idade + idade2)
-console.log(n1 + n2)
+console.log(n1 + n2) */
+
+//CODIGOS AULA 2 JS
+//Array
+
+//var lista = ["maca", "pera", "laranja"]
+//push para adicionar elementos a lista
+//lista.push("uva")
+//pop para remover elementos da lista
+//lista.pop("maca")
+//console.log(lista)
+//console.log(lista[1])
+//console.log(lista.reverse())
+//console.log(lista.length)
+//console.log(lista.toString())
+
+//console.log(lista[0])
+//console.log(lista.toString()[0])
+//Join para inserir elementos graficos na lista
+//console.log(lista.join(" | "))
+
+//DICIONARIOS
+
+/*
+var fruta = {nome: "maca", cor: "vermelha"}
+ console.log(fruta.nome)
+ console.log(fruta.cor)
+ */
+
+var frutas = [{nome: "maca", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas)
+console.log(frutas[1].cor)
+ 
