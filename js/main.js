7214a5728e27ee595bf2e022b1b268a248c7d3e4
iricardo97 <ricardodomingos97@gmail.com>
2020-06-04 21:40:41 -0300

    function setReplace add

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ function soma(n1, n2){
     return n1 + n2
 }
 
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
 alert(soma(5, 10))
+alert(setReplace("vai japao", "japao", "brasil"));
 
 
 
