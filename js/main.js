b52d89996c01e84b8dbb03c853f37462245e0858
iricardo97 <ricardodomingos97@gmail.com>
2020-06-04 21:56:48 -0300

    variaveis globais e locais javascript

--- js/main.js
+++ js/main.js
@@ -2,12 +2,19 @@ function soma(n1, n2){
     return n1 + n2
 }
 
-function setReplace(frase, nome, novo_nome){
-    return frase.replace(nome, novo_nome)
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
 }
 
-alert(soma(5, 10))
-alert(setReplace("vai japao", "japao", "brasil"));
+var idade = prompt("qual a sua idade?");
+console.log(validaIdade(idade));
+
 
 
 
